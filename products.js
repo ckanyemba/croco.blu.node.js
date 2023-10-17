4db60e59455f756f4e4b048859f7db7e857e1f5a
apadanag <95967301+ckanyemba@users.noreply.github.com>
2023-10-17 16:37:52 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id:1,
+        name: "iPhone 11 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image:"https://res.cloudinary.com/dyhkqsixw/image/upload/v1696782953/cld-sample-3.jpg",
+    },
+    {
+        id:2,
+        name: "iPhone 13 Pro",
+        desc: "6.1-inch display",
+        price: 1099,
+        image:"https://res.cloudinary.com/dyhkqsixw/image/upload/v1696782954/cld-sample-5.jpg",
+    },
+    {
+        id:3,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image:"https://res.cloudinary.com/dyhkqsixw/image/upload/v1696782951/cld-sample.jpg",
+    },
+];
+
+module.exports = products;
